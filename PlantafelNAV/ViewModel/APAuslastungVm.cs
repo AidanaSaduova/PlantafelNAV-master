2b6d937f1ffebca4ed043f52651c7192cdf9a40e
AidanaSaduova <wi16b102@technikum-wien.at>
2018-08-18 19:21:15 +0200

    BarChart XAML

--- PlantafelNAV/ViewModel/APAuslastungVm.cs
+++ PlantafelNAV/ViewModel/APAuslastungVm.cs
@@ -11,8 +11,18 @@ namespace PlantafelNAV.ViewModel
     public class APAuslastungVm:ViewModelBase
     {
         WS_Auf_Arb_Nav_Service ws_arbeitsplan = new WS_Auf_Arb_Nav_Service();
+        
 
-        //WS_Auf_Arb_Nav_Service[] list = ws_arbeitsplan.readmul
+
+        public void readData()
+        {
+            WS_Auf_Arb_Nav[] list = ws_arbeitsplan.ReadMultiple(null, null, 1000);
+            foreach (WS_Auf_Arb_Nav item in list)
+            {
+                //item.Ende =
+            }
+
+        }
 
     }
 }
