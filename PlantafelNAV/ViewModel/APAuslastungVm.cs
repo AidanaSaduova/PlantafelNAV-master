da4a7d674268eba0fa5d72dc89817646148d34c0
AidanaSaduova <wi16b102@technikum-wien.at>
2018-08-18 13:28:34 +0200

    Eigene Fehler schnell behoben

--- PlantafelNAV/ViewModel/APAuslastungVm.cs
+++ PlantafelNAV/ViewModel/APAuslastungVm.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PlantafelNAV.ws_aufarbservice;
 namespace PlantafelNAV.ViewModel
 {
+
     public class APAuslastungVm:ViewModelBase
     {
+        WS_Auf_Arb_Nav_Service ws_arbeitsplan = new WS_Auf_Arb_Nav_Service();
+
+        //WS_Auf_Arb_Nav_Service[] list = ws_arbeitsplan.readmul
+
     }
 }
--- PlantafelNAV/Views/APAuslastungView.xaml.cs
+++ PlantafelNAV/Views/APAuslastungView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using PlantafelNAV.TimelineNAV;
-using PlantafelNAV.ws_arbeitsplatzservice;
-using PlantafelNAV.ws_mbplantafel_service;
+using PlantafelNAV.ViewModel.Helpers;
+using PlantafelNAV.ws_aufarbservice;
 
 namespace PlantafelNAV.Views
 {
@@ -23,28 +24,11 @@ namespace PlantafelNAV.Views
     /// </summary>
     public partial class APAuslastungView : UserControl
     {
-        //Timelines erstellen - in diesem Fall werden diese dann als Auslastungsbalken dienen
-        //Tag = 8h -> 480 min -> 1% des Arbeitstages = 4,8 min 
-        Timeline t1 = new Timeline(1000, 80, 1);
-        Timeline t2 = new Timeline(1000, 80, 1);
-        Timeline t3 = new Timeline(1000, 80, 1);
-        Timeline t4 = new Timeline(1000, 80, 1);
-
         public APAuslastungView()
         {
-            
             InitializeComponent();
-            //erzeugen der Timelines für die max größe 
-            t1.Setup(28800, 57600, 3600, 120);
-            t2.Setup(28800, 57600, 3600, 120);
-            t3.Setup(28800, 57600, 3600, 120);
-            t4.Setup(28800, 57600, 3600, 120);
-
-            host1.Children.Add(t1);
-            host2.Children.Add(t2);
-            host3.Children.Add(t3);
-            host4.Children.Add(t4);
-
         }
+
+        
     }
 }
